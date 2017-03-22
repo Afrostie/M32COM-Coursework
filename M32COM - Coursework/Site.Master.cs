b9e93252e6043dae8f815800e19ec3a0654aa22f
DZ <opalcheneca@gmail.com>
2017-03-22 22:13:20 +0000

    Combining Login.aspx and Site.master.

--- M32COM - Coursework/Site.Master.cs
+++ M32COM - Coursework/Site.Master.cs
@@ -11,12 +11,15 @@ namespace M32COM___Coursework
 {
     public partial class Site : System.Web.UI.MasterPage
     {
+        //Object instances
         private UserUtilities userUtil;
         private CartUtilities cartUtil;
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            //Create the instance of CartUtilities class
             cartUtil = new CartUtilities();
+            //Create the instance of UserUtilities class
             userUtil = new UserUtilities();
 
             if (IsPostBack) return;
@@ -30,14 +33,32 @@ namespace M32COM___Coursework
                 Response.Redirect("Login.aspx");
         }
 
-        protected void Button1_Click(object sender, EventArgs e)
+        protected void LogIn_Click(object sender, EventArgs e)
+        {
+            //Try and login the User
+            if (userUtil.LoginUser(UserNameTB.Text, PasswordTB.Text))
+                // If User and Password are correct, move to another page
+                Response.Redirect("Default.aspx");
+            //Otherwise tell the user to register
+            else
+                Response.Write("User Does Not Exist, Please Register");
+        }
+
+        protected void Register_Click(object sender, EventArgs e)
+        {
+            if (userUtil.UserExists(TextBox1.Text))
+                Response.Write("User Exists");
+            userUtil.RegisterUser(TextBox1.Text, TextBox2.Text, TextBox4.Text, TextBox3.Text, TextBox5.Text, DropDownList1.SelectedValue);
+        }
+
+        protected void LogOut_Click(object sender, EventArgs e)
         {
             //When Logout button is clicked, logout and go back to login page
             userUtil.Logout();
             Response.Redirect("Login.aspx");
         }
 
-        protected void Button2_Click(object sender, EventArgs e)
+        protected void AddCart_Click(object sender, EventArgs e)
         {
             cartUtil.AddNewItemToCart(5, 1);
             cartUtil.AddNewItemToCart(5, 1);
--- M32COM - Coursework/Site.Master.designer.cs
+++ M32COM - Coursework/Site.Master.designer.cs
@@ -31,22 +31,103 @@ namespace M32COM___Coursework {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// ContentPlaceHolder1 control.
+        /// UserNameTB control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.TextBox UserNameTB;
         
         /// <summary>
-        /// Button1 control.
+        /// PasswordTB control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
+        protected global::System.Web.UI.WebControls.TextBox PasswordTB;
+        
+        /// <summary>
+        /// btnLogIn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLogIn;
+        
+        /// <summary>
+        /// TextBox1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox1;
+        
+        /// <summary>
+        /// TextBox2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox2;
+        
+        /// <summary>
+        /// TextBox3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox3;
+        
+        /// <summary>
+        /// TextBox4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox4;
+        
+        /// <summary>
+        /// TextBox5 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBox5;
+        
+        /// <summary>
+        /// DropDownList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+        
+        /// <summary>
+        /// btnRegister control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnRegister;
+        
+        /// <summary>
+        /// btnLogOut control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnLogOut;
         
         /// <summary>
         /// Label1 control.
@@ -76,13 +157,13 @@ namespace M32COM___Coursework {
         protected global::System.Web.UI.WebControls.Label CartLabel2;
         
         /// <summary>
-        /// Button2 control.
+        /// btnAddCart control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button2;
+        protected global::System.Web.UI.WebControls.Button btnAddCart;
         
         /// <summary>
         /// MainContent control.
