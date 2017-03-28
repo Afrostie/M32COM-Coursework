eeee5db8c348d3244241e361d65e9f51eed62d6f
Theodoros Charisis <charisit@uni.coventry.ac.uk>
2017-03-28 18:42:09 +0100

    commented out range validator

--- M32COM - Coursework/Products.aspx.designer.cs
+++ M32COM - Coursework/Products.aspx.designer.cs
@@ -148,15 +148,6 @@ namespace M32COM___Coursework {
         protected global::System.Web.UI.WebControls.Label lblCakeDescription;
         
         /// <summary>
-        /// RegularExpressionValidator2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator2;
-        
-        /// <summary>
         /// RequiredFieldValidator3 control.
         /// </summary>
         /// <remarks>
